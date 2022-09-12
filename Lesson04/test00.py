49e49dfe649c096a579a678996a42e10ad66a15d
Aidan Scott <93050512+ascot7@users.noreply.github.com>
2022-09-12 14:49:57 +0000

    Initial commit

---
+++ Lesson04/test00.py
@@ -0,0 +1,5 @@
+age = int(input("Enter your age:"))
+if age > 18:				# if you are over 18
+   print ("Too old for high school!") 
+else:					# if you are NOT over 18
+   print ("Too young for high school!")
---
+++ Lesson04/test01.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age:"))
+if age > 18:				# if you are over 18...
+   print ("Too old for school!" )
+elif age < 14:				# or under 14
+   print ("You must be in elementary!")
+else:					# anything else 
+   print ("Finally someone my age! ")
