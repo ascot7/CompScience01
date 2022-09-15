0230672b62115a243e3788d91473a5afe7cf0159
Aidan Scott <93050512+ascot7@users.noreply.github.com>
2022-09-15 13:38:17 +0000

    fix

---
+++ Lesson06/test06002.py
@@ -0,0 +1,18 @@
+print("Welcome to macOS Terminal!")
+print("Please refer to the user handbook for help.")
+print(" ")
+print(" ")
+
+loginUser = input("localhost login: ")
+loginPass = input("localhost password: ")
+
+if(loginUser == apple):
+  if(loginPass == alpine):
+    print("Login Success!")
+    print("Welcome to the macOS Kernel")
+    print(" ")
+    print("zsh>")
+  else:
+    print("Invalid Password")
+else:
+  print("This user does not exist")
